f175b6e15f3b2776daf6ee717f3253e1027106c8
xxwonderwallxx <vika010501@gmail.com>
2025-02-19 17:18:08 -0500

    - temp files remove

--- Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd749a73a7e94186dec94ecea0822858f6856af6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c2cc7b0bebf8d14bc085340a4b157502b9c916f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
