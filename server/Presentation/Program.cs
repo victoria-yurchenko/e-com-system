48d75406e80c14047617e140fd4543e5e73fbf6a
xxwonderwallxx <vika010501@gmail.com>
2025-02-20 01:21:15 -0500

    - backend main refactor

--- server/Presentation/Program.cs
+++ server/Presentation/Program.cs
@@ -1,16 +1,4 @@
-using Application.BackgroundServices;
-using Application.Interfaces;
-using Application.Services;
-using Infrastructure.DatabaseContext;
-using Infrastructure.PaymentGateways;
-using Infrastructure.Repositories;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.Identity;
-using Domain.Entities;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+using Presentation.Services;
 
 namespace Presentation
 {
@@ -18,90 +6,9 @@ namespace Presentation
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddScoped<IJwtService, JwtService>();
-            builder.Services.AddScoped<IAuthService, AuthService>();
-            builder.Services.AddScoped<ISubscriptionService, SubscriptionService>();
-            builder.Services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
-            builder.Services.AddScoped<IUserRepository, UserRepository>();
-            builder.Services.AddScoped<IPaymentGateway, StripePaymentGateway>();
-            builder.Services.AddScoped<INotificationService, NotificationService>();
-            builder.Services.AddScoped<IPaymentService, PaymentService>();
-            builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
-            builder.Services.AddScoped<IAdminService, AdminService>();
-            builder.Services.AddScoped<IAdminSubscriptionRepository, AdminSubscriptionRepository>();
-            builder.Services.AddScoped<IAnalyticsRepository, AnalyticsRepository>();
-            builder.Services.AddScoped<IAnalyticsService, AnalyticsService>();
-            builder.Services.AddScoped<IUserSubscriptionRepository, UserSubscriptionRepository>();
-
-            builder.Services.AddHostedService<SubscriptionRenewalService>();
-            builder.Services.Configure<HostOptions>(options =>
-            {
-                options.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.Ignore;
-            });
-
-            builder.Services.AddDataProtection()
-                .PersistKeysToFileSystem(new DirectoryInfo(@"/root/.aspnet/DataProtection-Keys"))
-                .SetApplicationName("SubscriptionManagementSystem");
-
-            builder.Services.AddControllers();
-
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(
-                    builder.Configuration.GetConnectionString("DefaultConnection"),
-                    sqlOptions => sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)));
-
-            builder.Services.AddIdentityCore<User>(options =>
-                       {
-                           // Здесь можно добавить настройки паролей и т.д.
-                           // options.Password.RequireDigit = true;
-                           // ...
-                       })
-                       .AddRoles<Role>()                // if need roles support
-                       .AddSignInManager<SignInManager<User>>() // if need SignInManager
-                       .AddEntityFrameworkStores<AppDbContext>()// for saving data in AppDbContext
-                       .AddDefaultTokenProviders();
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true,
-                        ValidateIssuerSigningKey = true,
-                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                        ValidAudience = builder.Configuration["Jwt:Audience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? string.Empty))
-                    };
-                });
-
-            builder.Logging.ClearProviders();
-            builder.Logging.AddConsole();
-            builder.Logging.AddDebug();
-
+            var builder = ConfigManager.CreateBuilder(args);
             var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
+            ConfigManager.ConfigureApplication(app);
             app.Run();
         }
     }
---
+++ server/Presentation/Services/ConfigManager.cs
@@ -0,0 +1,132 @@
+using Application.BackgroundServices;
+using Application.Interfaces;
+using Application.Services;
+using Infrastructure.DatabaseContext;
+using Infrastructure.PaymentGateways;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Presentation.Services
+{
+    public static class ConfigManager
+    {
+        public static WebApplicationBuilder CreateBuilder(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            ConfigureServices(builder);
+            ConfigureAuthentication(builder);
+            ConfigureLogging(builder);
+
+            return builder;
+        }
+
+        public static void ConfigureApplication(WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+        }
+
+        private static void ConfigureServices(WebApplicationBuilder builder)
+        {
+            RegisterServices(builder);
+            RegisterBackgroundServices(builder);
+            ConfigureDataProtection(builder);
+            ConfigureDatabase(builder);
+            ConfigureIdentity(builder);
+        }
+
+        private static void ConfigureAuthentication(WebApplicationBuilder builder)
+        {
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                        ValidAudience = builder.Configuration["Jwt:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"] ?? string.Empty))
+                    };
+                });
+        }
+
+        private static void RegisterServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddScoped<IJwtService, JwtService>();
+            builder.Services.AddScoped<IAuthService, AuthService>();
+            builder.Services.AddScoped<ISubscriptionService, SubscriptionService>();
+            builder.Services.AddScoped<ISubscriptionRepository, SubscriptionRepository>();
+            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            builder.Services.AddScoped<IPaymentGateway, StripePaymentGateway>();
+            builder.Services.AddScoped<INotificationService, NotificationService>();
+            builder.Services.AddScoped<IPaymentService, PaymentService>();
+            builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+            builder.Services.AddScoped<IAdminService, AdminService>();
+            builder.Services.AddScoped<IAdminSubscriptionRepository, AdminSubscriptionRepository>();
+            builder.Services.AddScoped<IAnalyticsRepository, AnalyticsRepository>();
+            builder.Services.AddScoped<IAnalyticsService, AnalyticsService>();
+            builder.Services.AddScoped<IUserSubscriptionRepository, UserSubscriptionRepository>();
+        }
+
+        private static void RegisterBackgroundServices(WebApplicationBuilder builder)
+        {
+            builder.Services.AddHostedService<SubscriptionRenewalService>();
+            builder.Services.Configure<HostOptions>(options =>
+            {
+                options.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.Ignore;
+            });
+        }
+
+        private static void ConfigureDataProtection(WebApplicationBuilder builder)
+        {
+            builder.Services.AddDataProtection()
+              .PersistKeysToFileSystem(new DirectoryInfo(builder.Configuration["DataProtection:KeysPath"]))
+              .SetApplicationName(builder.Configuration["App:Name"]);
+        }
+
+        private static void ConfigureDatabase(WebApplicationBuilder builder)
+        {
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection"),
+                    sqlOptions => sqlOptions.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null)));
+        }
+
+        private static void ConfigureIdentity(WebApplicationBuilder builder)
+        {
+            builder.Services.AddIdentityCore<User>(options =>
+            {
+                // password setup i.e.:
+                // options.Password.RequireDigit = true;
+            })
+                .AddRoles<Role>()
+                .AddSignInManager<SignInManager<User>>()
+                .AddEntityFrameworkStores<AppDbContext>()
+                .AddDefaultTokenProviders();
+        }
+
+        private static void ConfigureLogging(WebApplicationBuilder builder)
+        {
+            builder.Logging.ClearProviders();
+            builder.Logging.AddConsole();
+            builder.Logging.AddDebug();
+        }
+    }
+}
